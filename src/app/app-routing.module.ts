6a417323595214368901138af8a31e275fd2030a
LucasHartman <lucas.hartman@kognitive.ca>
2023-06-28 15:15:04 -0400

    Update names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from "./projects/projects.component";
 import { ResearchComponent } from "./research/research.component";
 import { HomeComponent } from "./home/home.component";
-import { ContactComponent } from "./contact/contact.component";
 
 const routes: Routes = [{
-  path: 'home', component: HomeComponent
+  path: 'about', component: HomeComponent
 }, {
   path: 'projects', component: ProjectsComponent
 }, {
   path: 'research', component: ResearchComponent
 }, {
-  path: 'contact', redirectTo: '/home', pathMatch: 'full'
+  path: 'contact', redirectTo: '/about', pathMatch: 'full'
 }, {
-  path: '', redirectTo: '/home', pathMatch: 'full'
+  path: '', redirectTo: '/about', pathMatch: 'full'
 }];
 
 @NgModule({
